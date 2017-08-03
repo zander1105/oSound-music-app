46d30c82ae8397d6b9a39ab114ea3b21e0f4ba63
zander <aosborne2018@myshs.org.uk>
2017-08-03 11:02:56 +0100

    made artists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801114859) do
+ActiveRecord::Schema.define(version: 20170802153112) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "title"
